8d36e0882fedbc78d015035729e41c0368b23ea4
Marcos R Oliveira <mroger.oliveira@gmail.com>
2016-03-14 19:22:42 -0300

    Application for integration test

---
+++ fiveware-test-model/src/test/java/br/com/cpim/DBTestApplication.java
@@ -0,0 +1,18 @@
+package br.com.cpim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe criada para exercitar o banco de dados e os mapeamentos JPA isoladamente da aplicacao.
+ * 
+ * @author Roger
+ *
+ */
+@SpringBootApplication
+public class DBTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DBTestApplication.class);
+	}
+}
