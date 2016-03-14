bd7b14195a61e8c36b1f82a6ffbe1f690efeebef
Marcos R Oliveira <mroger.oliveira@gmail.com>
2016-03-14 19:18:52 -0300

    Adding stereotypes

--- fiveware-test-model/src/main/java/br/com/cpim/repository/AccessoryRepository.java
+++ fiveware-test-model/src/main/java/br/com/cpim/repository/AccessoryRepository.java
@@ -1,9 +1,11 @@
 package br.com.cpim.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.cpim.model.Accessory;
 
+@Repository
 public interface AccessoryRepository extends JpaRepository<Accessory, Integer> {
 
 }
--- fiveware-test-model/src/main/java/br/com/cpim/repository/RentalRepository.java
+++ fiveware-test-model/src/main/java/br/com/cpim/repository/RentalRepository.java
@@ -1,9 +1,11 @@
 package br.com.cpim.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.cpim.model.Rental;
 
+@Repository
 public interface RentalRepository extends JpaRepository<Rental, Long> {
 
 }
